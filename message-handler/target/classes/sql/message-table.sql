6d7eddf4299d90882eef9e7e45658a77ae4b4b76
Balag <Balagurovskiyos@gmail.com>
2024-08-04 23:34:36 +0300

    target delete

--- message-handler/target/classes/sql/message-table.sql
+++
@@ -1,4 +0,0 @@
-drop table messages;
-create table messages(id bigint auto_increment, message varchar(255), createDt TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
-insert into messages(message) values('test 1');
-select * from messages;
